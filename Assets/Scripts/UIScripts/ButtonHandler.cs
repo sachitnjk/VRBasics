b88128c284db0c2f19a8eae385e466125ea7f71f
Sachit Nanajkar <sachitnjk@gmail.com>
2023-12-03 17:35:19 +0000

    Scene setup for Main menu done

---
+++ Assets/Scripts/UIScripts/ButtonHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+	public void StartGame()
+	{
+
+	}
+	public void ToSettings()
+	{
+
+	}
+	public void Exitgame()
+	{
+		Debug.Log("qutting application");
+		Application.Quit();
+	}
+}
