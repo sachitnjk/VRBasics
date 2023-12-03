6a311acb01bc5f9d5116565af43f13a24ea35014
Sachit Nanajkar <sachitnjk@gmail.com>
2023-12-03 19:56:53 +0000

    MainMenu button flow done

--- Assets/Scripts/UIScripts/ButtonHandler.cs
+++ Assets/Scripts/UIScripts/ButtonHandler.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
-	public void StartGame()
-	{
+	[SerializeField] private GameObject settingsPanel;
+	[SerializeField] private GameObject mainMenuPanel;
+	[SerializeField] private GameObject audioPanel;
+	[SerializeField] private GameObject controlsPanel;
 
+	public void StartGame(int index)
+	{
+		SceneManager.LoadScene(index);
 	}
 	public void ToSettings()
 	{
-
+		mainMenuPanel.SetActive(false);
+		settingsPanel.SetActive(true);
 	}
+
+	public void ToAudioPanel()
+	{
+		settingsPanel.SetActive(false);
+		audioPanel.SetActive(true);
+	}
+
+	public void ToControlsPanel()
+	{
+		settingsPanel.SetActive(false);
+		controlsPanel.SetActive(true);
+	}
+
+	public void ReturnToMainMenu()
+	{
+		settingsPanel.SetActive(false);
+		mainMenuPanel.SetActive(true);
+	}
+
+	public void ReturnToSettings(GameObject activePanel)
+	{
+		activePanel.SetActive(false);
+		settingsPanel.SetActive(true);
+	}
+
 	public void Exitgame()
 	{
 		Debug.Log("qutting application");
