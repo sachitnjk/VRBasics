84e790798d8bdfcd0de23814f1a942386e4a6f1e
Sachit Nanajkar <sachitnjk@gmail.com>
2023-12-10 12:46:22 +0000

    Build_ver1.0.1, Added settings volume slider functionality

--- Assets/Scripts/UIScripts/ButtonHandler.cs
+++ Assets/Scripts/UIScripts/ButtonHandler.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
+	[Header("Panel references")]
 	[SerializeField] private GameObject settingsPanel;
 	[SerializeField] private GameObject mainMenuPanel;
 	[SerializeField] private GameObject audioPanel;
 	[SerializeField] private GameObject controlsPanel;
+
+	[Header("AudioMixer")]
+	[SerializeField] private AudioMixer audioMixer;
 
 	public void StartGame(int index)
 	{
@@ -48,5 +53,20 @@ public class ButtonHandler : MonoBehaviour
 	{
 		Debug.Log("qutting application");
 		Application.Quit();
+	}
+
+	//Slider Functions
+
+	public void MasterVolumeSetter(float slidervalue)
+	{
+		audioMixer.SetFloat("MasterVolume", Mathf.Log10(slidervalue) * 20);
+	}
+	public void EntityVolumeSetter(float slidervalue)
+	{
+		audioMixer.SetFloat("EntityVolume", Mathf.Log10(slidervalue) * 20);
+	}
+	public void BGMSetter(float slidervalue)
+	{
+		audioMixer.SetFloat("BGM", Mathf.Log10(slidervalue) * 20);
 	}
 }
